33fad49f313c961adf8b89cbcce2a13e6762091d
Mike Lysikov <d8corp@mail.ru>
2020-11-21 21:35:11 +0300

    release v1.0.0

---
+++ lib/index.d.ts
@@ -0,0 +1,17 @@
+import Async, { IAsyncOptions } from '@watch-state/async';
+declare type AjaxQueryType = string | number | boolean;
+export declare type AjaxQuery = AjaxQueryType | AjaxQueryType[];
+export declare type AjaxQueryObject = Record<string, AjaxQuery>;
+export declare type AjaxData = Record<string, string | number>;
+export declare type AjaxOptions<D extends AjaxData = AjaxData, Q extends AjaxQueryObject = AjaxQueryObject> = RequestInit & Omit<IAsyncOptions, 'request'> & {
+    data?: D;
+    query?: Q;
+    type?: 'json' | 'text' | 'blob' | 'arrayBuffer' | 'formData';
+};
+declare class Ajax<V = any, E = any, D extends AjaxData = AjaxData, Q extends AjaxQueryObject = AjaxQueryObject> extends Async<V, E> {
+    query: Q;
+    data: D;
+    answer: Response;
+    constructor(url: string, options?: AjaxOptions<D, Q>);
+}
+export default Ajax;
---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import Async, {IAsyncOptions} from '@watch-state/async'
+import {state, createEvent} from 'watch-state'
+
+type AjaxQueryType = string | number | boolean
+export type AjaxQuery = AjaxQueryType | AjaxQueryType[]
+export type AjaxQueryObject = Record<string, AjaxQuery>
+export type AjaxData = Record<string, string | number>
+export type AjaxOptions <D extends AjaxData = AjaxData, Q extends AjaxQueryObject = AjaxQueryObject> = RequestInit & Omit<IAsyncOptions, 'request'> & {
+  data?: D
+  query?: Q
+  type?: 'json' | 'text' | 'blob' | 'arrayBuffer' | 'formData'
+}
+
+class Ajax<V = any, E = any, D extends AjaxData = AjaxData, Q extends AjaxQueryObject = AjaxQueryObject> extends Async<V, E> {
+  query: Q
+  data: D
+  @state answer: Response
+
+  constructor (url: string, options: AjaxOptions<D, Q> = {}) {
+    const {type = 'text'} = options
+
+    const request = (resolve, reject) => {
+      let query = ''
+      for (const key in this.query) {
+        const value = this.query[key]
+        if (!value) continue
+        if (Array.isArray(value)) {
+          for (const subValue of value) {
+            query += (query && '&') + key + '[]=' + subValue
+          }
+        } else {
+          query += (query && '&') + key + '=' + value
+        }
+      }
+      if (query) {
+        query = '?' + query
+      }
+      let answer
+      fetch(url.replace(/\{(\w+)\}/g, (str, key) => this.data[key] + '') + query, options).then(data => {
+        answer = data
+        return data[type]()
+      }).then(createEvent(data => {
+        this.answer = answer
+        answer.status > 399 ? reject(data) : resolve(data)
+      }), createEvent(e => {
+        this.answer = answer
+        reject(e)
+      }))
+    }
+
+    super({...options, request})
+
+    this.query = options.query || {} as Q
+    this.data = options.data || {} as D
+  }
+}
+
+export default Ajax
